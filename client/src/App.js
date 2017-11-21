6d7e2751f5291191df3c42613d7781278d4e889a
Francesco Giordano <francesco.giordano@buildo.io>
2017-11-21 17:48:07 +1100

    shorten urls in message and send sms

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,21 @@ import {
 
 const Input = (props) => <DumbInput {...props} onChange={e => { props.onChange(e.target.value) }} />
 
+// https://stackoverflow.com/questions/8188645/javascript-regex-to-match-a-url-in-a-field-of-text
+const urlMatchRe = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?/g
+
+const shortenUrl = (url) => fetch(`/shorten?url=${url}`).then(r => r.json()).then(r => r.shortenedUrl)
+
+const replaceUrls = (message) => {
+  const longUrls = message.match(urlMatchRe) || []
+  return Promise.all(longUrls.map(shortenUrl))
+    .then((shortUrls) => message.replace(urlMatchRe, (longUrl) => shortUrls[longUrls.indexOf(longUrl)]))
+}
+
+const sendSMS = ({ message, from, to }) => {
+  return fetch(`/sms?from=${from}&to=${to}&message=${message}`).then(r => r.json())
+}
+
 const initialPhoneNumber = '61406369533'
 const initialMessage1 = 'Hi Francesco, we want to hire you! Just kidding, we just forgot to change the default message and recipient. Check out this video https://www.youtube.com/watch?v=NHo7fSJ9ItE'
 const initialSender = 'BurstSMS'
@@ -35,7 +50,8 @@ const AppPureUI = ({
   sender,
   updateMessage,
   updatePhoneNumber,
-  updateSender
+  updateSender,
+  sendMessage
 }) => (
   <Form>
 
@@ -72,7 +88,7 @@ const AppPureUI = ({
       />
     </FormGroup>
 
-    <Button color='primary' size='lg' block onClick={() => { alert('not yet!') }}>
+    <Button color='primary' size='lg' block onClick={sendMessage}>
       SEND MESSAGE
     </Button>
 
@@ -89,8 +105,24 @@ export default class App extends Component {
     updateSender: (newValue) => { this.setState(updateSender(newValue)) }
   }
 
+  actions = {
+    sendMessage: () => {
+      return replaceUrls(this.state.message)
+        .then(newMessage => {
+          return sendSMS({ message: newMessage, from: this.state.sender, to: this.state.phoneNumber })
+        })
+        .then(response => {
+          if (response.error.description === 'OK') {
+            alert('message sent')
+          } else {
+            alert('message might not be sent ¯\_(ツ)_/¯') // eslint-disable-line no-useless-escape
+          }
+        })
+    }
+  }
+
   render() {
-    return <AppPureUI {...this.state} {...this.updaters} />
+    return <AppPureUI {...this.state} {...this.updaters} {...this.actions} />
   }
 }
 
